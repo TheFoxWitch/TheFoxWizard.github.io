4c58afa0c7ed887e455a61606563fe611a642ec0
UnimportantWank <Xeromancer@gmail.com>
2016-12-22 18:27:32 -0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        $(".top").css("opacity", 1.1 - $(window).scrollTop() / 250);
+    });
+    $(window).scroll(function() {
+        $("#about").css("opacity",2.65 - $(window).scrollTop() / 360);
+    });
