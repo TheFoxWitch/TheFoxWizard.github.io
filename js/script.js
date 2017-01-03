779ff5ee41424de4a865711422acea1eef40d69d
UnimportantWank <Xeromancer@gmail.com>
2017-01-02 22:39:50 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
         $(".top").css("opacity", 1.1 - $(window).scrollTop() / 250);
     });
     $(window).scroll(function() {
-        $("#about").css("opacity",2.75 - $(window).scrollTop() / 320);
+        $("#about").css("opacity",2.75 - $(window).scrollTop() / 300);
     });
 });
